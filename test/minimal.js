d36f82819a12cc096cf24df76807117b558b7f32
Wes <averagewizard13@gmail.com>
2021-07-16 13:39:39 -0600

    Add minimal test to demonstrate basics

---
+++ test/minimal.js
@@ -0,0 +1,13 @@
+const { Client, Room } = require('../src')
+const client = new Client()
+
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.email}!`)
+  await client.joinRoom(63978621038107, { nickname: 'Deiga', color: 'FF3333' })
+})
+
+client.on('message', async message => {
+  if (message.content === 'ping') message.room.send('pong')
+})
+
+client.login(process.env.EMAIL, process.env.PASSWORD).catch(console.error)
